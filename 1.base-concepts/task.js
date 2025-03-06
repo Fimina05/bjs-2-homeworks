a4f740d112524f64932a47dcee0cc05a93a5e73a
Fimina05 <115074342+Fimina05@users.noreply.github.com>
2025-03-07 00:58:28 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
-"use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let D = b ** 2 - 4 * a * c;
+
+  if (D < 0) {
+    return arr;
+  } else if (D === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  } else {
+    let x1 = (-b + Math.sqrt(D)) / (2 * a);
+    let x2 = (-b - Math.sqrt(D)) / (2 * a);
+    arr.push(x1, x2);
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let P = (percent / 100) / 12;
+  let S = amount - contribution;
+  let monthlyPayment = S * (P + (P / ((1 + P) ** countMonths - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+
+  return Number(totalAmount.toFixed(2));
 }
