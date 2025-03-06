46f0502912b6a8cca45b658711450b9b77473474
flora <fimina2003@inbox.ru>
2025-03-07 02:55:02 +0300

    Добавлены task.js и tests.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,22 +1,21 @@
 function getArrayParams(...arr) {
   if (arr.length === 0) return { min: 0, max: 0, avg: 0 };
-  
+
   let min = arr[0];
   let max = arr[0];
   let sum = 0;
 
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-    if (arr[i] < min) min = arr[i];
-    if (arr[i] > max) max = arr[i];
+  for (let num of arr) {
+      if (num < min) min = num;
+      if (num > max) max = num;
+      sum += num;
   }
 
-  let avg = (sum / arr.length).toFixed(2);
-  return { min, max, avg: +avg }; // Преобразуем обратно в число
+  return { min, max, avg: Number((sum / arr.length).toFixed(2)) };
 }
 
 function summElementsWorker(...arr) {
-  return arr.reduce((sum, current) => sum + current, 0);
+  return arr.reduce((sum, num) => sum + num, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
@@ -25,21 +24,49 @@ function differenceMaxMinWorker(...arr) {
 }
 
 function differenceEvenOddWorker(...arr) {
-  let evenSum = 0, oddSum = 0;
-  arr.forEach(num => {
-    if (num % 2 === 0) evenSum += num;
-    else oddSum += num;
-  });
-  return evenSum - oddSum;
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+
+  for (let num of arr) {
+      if (num % 2 === 0) sumEvenElement += num;
+      else sumOddElement += num;
+  }
+
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
-  let evenElements = arr.filter(num => num % 2 === 0);
-  if (evenElements.length === 0) return 0;
-  return evenElements.reduce((sum, num) => sum + num, 0) / evenElements.length;
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+
+  for (let num of arr) {
+      if (num % 2 === 0) {
+          sumEvenElement += num;
+          countEvenElement++;
+      }
+  }
+
+  return countEvenElement ? sumEvenElement / countEvenElement : 0;
 }
 
 function makeWork(arrOfArr, func) {
-  let results = arrOfArr.map(arr => func(...arr));
-  return Math.max(...results);
+  if (arrOfArr.length === 0) return 0;
+
+  let maxWorkerResult = -Infinity;
+
+  for (let arr of arrOfArr) {
+      const result = func(...arr);
+      if (result > maxWorkerResult) maxWorkerResult = result;
+  }
+
+  return maxWorkerResult;
 }
+
+module.exports = { 
+  getArrayParams, 
+  summElementsWorker, 
+  differenceMaxMinWorker, 
+  differenceEvenOddWorker, 
+  averageEvenElementsWorker, 
+  makeWork 
+};
